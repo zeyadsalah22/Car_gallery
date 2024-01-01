23806c3f17240677849ecc62cde4e09a1ebfcaa8
Ahmed-waled <103792966+Ahmed-waled@users.noreply.github.com>
2024-01-01 21:03:15 +0200

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,59 @@
+public class Employee extends User{
+    String position;
+    double salary;
+    int daysAttend;
+    Employee(){
+
+    }
+
+    public Employee(String firstName, String lastName, int age, boolean married, String position, double salary, int daysAttend, int id) {
+        super(firstName, lastName, age, id, married);
+        this.position = position;
+        this.salary = salary;
+        this.daysAttend = daysAttend;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public int getDaysAbsent() {
+        return daysAttend;
+    }
+
+    public void setDaysAbsent(int daysAttend) {
+        this.daysAttend = daysAttend;
+    }
+
+    public void attend(){
+        this.daysAttend++;
+    }
+    public void updatePosition(){
+        this.position = "Manager";
+    }
+    public double calculateSalary(){
+        return this.salary * daysAttend / 26f;
+    }
+
+    public void displayUser(){
+        super.displayUser();
+        System.out.printf("""
+                Position: %s
+                Salary: %f
+                Days attend: %d
+                Expected Salary: %f
+                """,this.position, this.salary, this.daysAttend, this.calculateSalary());
+    }
+}
